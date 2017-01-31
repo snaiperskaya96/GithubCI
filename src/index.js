ae29e1b343dae919c86c6e322fadd8e985828eb7
Jury Verrigni <jury.verrigni@gmail.com>
2017-01-31 10:11:26 +0000

    Downloading ubuntu image if it doesn't exist

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
+const dockerClient = require('./clients/docker');
+const fs = require('fs');
 global._ = require('underscore');
+
+dockerClient.listImages((error, list) => {
+	isUbuntuPresent = false;
+	list.forEach(image => {
+		if (image.RepoTags && image.RepoTags.indexOf('ubuntu:latest') > -1) {
+			isUbuntuPresent = true;
+		}
+	});
+
+	if (!isUbuntuPresent) {
+		console.log ('Downloading ubuntu image, please wait, it will only takes a few minutes');
+		dockerClient.createImage({fromImage: 'ubuntu:latest'}, (err, stream) => {
+			const writeStream = fs.createWriteStream('/tmp/GithubCI.log');
+			stream.pipe(writeStream, {end: true});
+			console.log('Please don\'t trigger any Pull Request...')
+	        stream.on('end', () => {
+				console.log('Done! You\'re now good to go!');
+	        });
+		});
+	}
+});
+
+
 let server = require('./restify');
